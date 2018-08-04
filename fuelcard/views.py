2ae9a2411b92f4df83787ecb36243a9db5d26f40
Ashaba <jnashaba4@gmail.com>
2018-08-04 14:49:26 +0300

    fix static files

--- fuelcard/views.py
+++ fuelcard/views.py
@@ -1,10 +1,7 @@
-#fuelcard/views.py
-
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-	
-	# Create your views here.
+
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'sufee/index.html', context=None)
+        return render(request, 'home.html', context=None)
